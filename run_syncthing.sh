3a1ea23696c0e8712c48c4b522a69561342ff965
Andrej Nankov <nanorocks@outlook.com>
2018-01-30 21:37:38 +0100

    Update run_syncthing.sh

--- run_syncthing.sh
+++ run_syncthing.sh
@@ -37,7 +37,7 @@ function remove_docker_image()
 function build_docker_containers()
 {
 
-	#docker run -v /config --name nanorocks_syncthing_amd64_config amd64/ubuntu chown -R 22000 /config
+	docker run -v /config --name nanorocks_syncthing_amd64_config amd64/ubuntu chown -R 22000 /config
 	# Create config volume and set permissions:
 	docker run -d --net='host' -v /mnt/media:/mnt/media --volumes-from nanorocks_syncthing_amd64_config \
 	-p 22000:22000 -p 8384:8384 -p 21027:21027/udp --name nanorocks_syncthing_amd64 nanorocks/syncthing_nanorocks_amd64
